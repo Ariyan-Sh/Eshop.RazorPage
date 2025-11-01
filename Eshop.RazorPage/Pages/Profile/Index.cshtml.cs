4abbe5c326b71dbc633011d2f677972623328b42
Ariyan <ariyanshirzadi3939@gmail.com>
2025-11-01 14:26:31 -0700

    adjusting user name and family in header and adding unobtrusive package and edititng profile index

--- Eshop.RazorPage/Pages/Profile/Index.cshtml.cs
+++ Eshop.RazorPage/Pages/Profile/Index.cshtml.cs
@@ -1,18 +1,36 @@
+using Eshop.RazorPage.Models.Users;
+using Eshop.RazorPage.Services.Users;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace Eshop.RazorPage.Pages.Profile
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserService _userService;
+
+        public IndexModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public UserDto CurrentUser { get; set; }
+        public async Task<IActionResult> OnGet()
         {
             if (!User.Identity.IsAuthenticated)
             {
-                Redirect("/Auth/Login?redirectTo=Profile");
-                return;
+                return Redirect("/Auth/Login?redirectTo=Profile");
             }
+
+            CurrentUser = await _userService.GetCurrentUser();
+
+            if(CurrentUser == null)
+            {
+                return Redirect("/Auth/Login?redirectTo=Profile");
+            }
+            return Page();
         }
     }
 }
